371476b6677f3ca4358b2caaded6a56f1c265dbb
Brian Bassett <brianbassett@brians-mbp.mynetworksettings.com>
2022-06-09 13:33:58 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Python GUI
+
+## Setup
+
+- clone repository and download onto your computer
+- open gui-python file and click the 'run' or 'play button
+- conversely, open with your CLI by typing "python gui-python.py" (may be "python3 ...")
---
+++ gui-python.py
@@ -0,0 +1,24 @@
+from logging import root
+import tkinter as tk
+from tkinter import Canvas, filedialog, Text
+import os
+
+root = tk.Tk()
+
+def addApp():
+    filename= filedialog.askopenfilename(initialdir="/", title="Select File",
+    filetypes=(("executable", "*.exe"), ("all files", "*.*")))
+
+canvas = tk.Canvas(root, height=700, width=700, bg="#263d42")
+canvas.pack()
+
+frame = tk.Frame(root, bg="white")
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+
+openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="black", bg="#263D42", command=addApp)
+openFile.pack()
+
+runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="black", bg="#263D42")
+runApps.pack()
+
+root.mainloop()
